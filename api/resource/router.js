5abbc9a103b1c0184827428c2ba0a3f6f534fbea
BillyBernard <billybernard54@gmail.com>
2021-11-05 10:07:49 -0600

    resource works

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,23 @@
 // build your `/api/resources` router here
 const express = require("express");
-
 const router = express.Router();
+
+const Resources = require('./model')
+
+router.get('/', (req, res, next) => {
+    Resources.getResources()
+        .then(resourceList => {
+            res.json(resourceList)
+        })
+        .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+    Resources.postResource(req.body)
+        .then(insertedResource => {
+            res.status(201).json(insertedResource)
+        })
+        .catch(next);
+})
 
 module.exports = router;
