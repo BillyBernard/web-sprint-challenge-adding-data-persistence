506859a7aae7955a513d2e691ccb0bda0cc2ec74
BillyBernard <billybernard54@gmail.com>
2021-11-05 10:34:44 -0600

    project passed

--- api/project/router.js
+++ api/project/router.js
@@ -1,8 +1,24 @@
 // build your `/api/projects` router here
 const express = require("express");
-
 const router = express.Router();
 
+const Projects = require('./model')
 
+router.get('/', (req, res, next) => {
+    Projects.getProjects()
+        .then(projectList => {
+            res.json(projectList)
+        })
+        .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+    Projects.postProject(req.body)
+        .then(insertedProject => {
+            res.status(201).json(insertedProject)
+        })
+        .catch(next);
+})
 
 module.exports = router;
+
