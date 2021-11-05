7b7c48e7a9fd768da96a76059c01b27419b5c157
BillyBernard <billybernard54@gmail.com>
2021-11-05 10:00:56 -0600

    tasks passing

--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,23 @@
 // build your `/api/tasks` router here
 const express = require("express");
+const Tasks = require('./model');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Tasks.getTasks()
+        .then(tasksList => {
+            res.json(tasksList)
+        })
+        .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+    Tasks.postTasks(req.body)
+        .then(insertedTask => {
+            res.status(201).json(insertedTask)
+        })
+        .catch(next);
+})
 
 module.exports = router;
